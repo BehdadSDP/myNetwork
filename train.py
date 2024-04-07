2d1431a73d406ca1cccffbda3e5d562bdd158ca5
BehdadSDP <sadeghianbehdad@gmail.com>
2024-04-07 20:40:56 +0330

    add size inf

--- train.py
+++ train.py
@@ -353,6 +353,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 loss, loss_items = compute_loss(pred, targets.to(device))# loss scaled by batch_size
                 #add dehaze loss to total loss
                 loss = loss + d_loss
+                print("loss size:", loss.size())
+                print("d_loss size:", d_loss.size())
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
                 if opt.quad:
