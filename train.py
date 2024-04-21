e1e02fc0d4061bed264a8dacfebf58677f85ca25
BehdadSDP <sadeghianbehdad@gmail.com>
2024-04-21 12:35:39 +0330

    add d_loss log

--- train.py
+++ train.py
@@ -285,7 +285,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             train_loader.sampler.set_epoch(epoch)
 
         pbar = enumerate(train_loader)
-        LOGGER.info(('\n' + '%11s' * 6  % ('Epoch', 'GPU_mem', 'box_loss', 'obj_loss', 'cls_loss', 'loss')))
+        LOGGER.info(('\n' + '%11s' * 7  % ('Epoch', 'GPU_mem', 'box_loss', 'obj_loss', 'cls_loss', 'loss', 'd_loss')))
         if RANK in {-1, 0}:
             pbar = tqdm(pbar, total=nb, bar_format=TQDM_BAR_FORMAT)  # progress bar
         optimizer.zero_grad()
@@ -342,8 +342,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             if RANK in {-1, 0}:
                 mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
                 mem = f'{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G'  # (GB)
-                pbar.set_description(('%11s' * 2 + '%11.4g' * 4) %
-                                     (f'{epoch}/{epochs - 1}', mem, *mloss, loss.item(), ))
+                pbar.set_description(('%11s' * 2 + '%11.4g' * 5) %
+                                     (f'{epoch}/{epochs - 1}', mem, *mloss, loss.item(), d_loss.item()))
                 callbacks.run('on_train_batch_end', model, ni, imgs, targets, paths, list(mloss))
                 if callbacks.stop_training:
                     return
